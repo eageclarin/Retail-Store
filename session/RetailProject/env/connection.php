9adf5a693127080fe588128d5a79e83d2b98b7d4
emreyno <elymerreyno@gmail.com>
2021-11-20 23:48:37 +0800

    with eigram code

---
+++ session/RetailProject/env/connection.php
@@ -0,0 +1,46 @@
+<?php
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+
+    //create conection
+    $conn = new mysqli($servername, $username, "");
+
+    //check connection
+    if($conn -> connect_errno){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+		exit();
+	}
+
+    //create database if not exists
+	$db = "CMSC127RetailProject";
+	$sql = "CREATE DATABASE IF NOT EXISTS $db";
+	if (mysqli_query($conn, $sql)){
+		mysqli_select_db($conn, $db); //connect to database after database created 
+        //if success call connection.php
+
+        //create tables
+        $query = '';
+        $sqlDB = file('db.sql');
+        foreach ($sqlDB as $line)	{
+            $startWith = substr(trim($line), 0 ,2);
+            $endWith = substr(trim($line), -1 ,1);
+            
+            if (empty($line) || $startWith == '--') {
+                continue;
+            }
+                
+            $query = $query . $line;
+            if ($endWith == ';') {
+                mysqli_query($conn,$query);
+                $query= '';
+            } else {
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+            }
+        }
+	} else {
+		echo "ERROR: Could not be able to execute $sql." . mysqli_error($conn);
+	}
+
+	
+?>
