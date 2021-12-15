23a2fb5230ca0bf5b5b276db2b4fc19d15a266e9
JaeCamp00 <jlcampo@up.edu.ph>
2021-12-15 11:22:59 +0800

    fixed idle 10 mins

--- session/RetailProject/client/update.php
+++ session/RetailProject/client/update.php
@@ -1,5 +1,5 @@
 <?php
-	include_once '../env/userconnection.php';
+	include_once '../env/userConnection.php';
 
 	$id = $item = $qty = $branch = "";
 	if (isset($_GET['id']) && isset($_GET['item']) && isset($_GET['qty']) && isset($_GET['branch'])) {
