01846adc8542d1eedcf52f2eb7c1683558bc3a38
emreyno <elymerreyno@gmail.com>
2021-12-16 13:38:25 +0800

    new update

--- session/RetailProject/admin/adminHome.php
+++ session/RetailProject/admin/adminHome.php
@@ -41,9 +41,115 @@ if ($branchID_Check>0) {                                               #username
     
     <div class="container-fluid     mt-4 pt-4 pb-4 ps-4 pe-4">
         <div class="row align-items-start">
-          <div class="col ">
+        <div class="col  bg-dark p-2 text-dark bg-opacity-10 rounded">
+          <div class="col  bg-transparent ms-2 me-2 p-2 text-dark  rounded">
+          <h1 class="text-center">General Manager</h1>
+          <table class="table table-striped table-hover fs-6  fw-normal">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Contact</th>
+                      
             
+
+                        
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+                
+                       
+                        
+                         $query = "SELECT * FROM admin natural join admin_contact where admin.admin_ID=1;"; 
+                         $result = mysqli_query($conn,$query);
+                         $Check = mysqli_num_rows($result);
+                      
+                            if ( $Check>0) {                                                       
+                                while($row = mysqli_fetch_assoc($result)) {
+                                  echo"<tr>
+                                  <td>".$row['admin_Username']."</td>
+                                  <td>". $row['contact'] ." </td>
+                      
+                                  </tr>";
+                          
+                                }
+                            } 
+
+                        
+
+                            
+                            
+                          
+                            
+                        ?>
+                         </tbody>
+            </table>  
           </div>
+            <h1 class="text-center">Branch Managers</h1>
+
+            <?php 
+
+            if($_SESSION['admin']==1){  ?>
+            <div class="fs-6">
+            <h5 > <span class="btn badge btn-dark" onclick="managerForm()" >Add Manager</span></h5 >
+            </div>
+           
+
+              
+            <?php 
+
+            }
+            
+            ?>
+
+            <table class="table table-striped table-hover fs-6  fw-normal">
+                <thead>
+                    <tr>
+                        <th scope="col">Branch Name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Contact</th>
+                        <th scope="col">Manager</th>
+                      
+            
+
+                        
+                    </tr>
+                </thead>
+                <tbody>
+            <?php
+                
+                       
+                        
+                         $query = "SELECT * FROM branch natural join a_manages_b natural join branch_contact natural join admin where admin.admin_ID>1;"; 
+                         $result = mysqli_query($conn,$query);
+                         $Check = mysqli_num_rows($result);
+                      
+                            if ( $Check>0) {                                                       
+                                while($row = mysqli_fetch_assoc($result)) {
+                                  echo"<tr>
+                                  <td>".$row['branch_Name']."</td>
+                                  <td>". $row['branch_Address'] ." </td>
+                                  <td>". $row['contact'] ." </td>
+                                  <td>"; ?> <button type="button" class="badge btn btn-secondary" onclick="showManager(<?php  echo $row['admin_ID'] ;?>)" >See Info</button></td>
+                                  <?php echo "</td>
+                                                          
+                                  </tr>";
+                          
+                                }
+                            } 
+
+                        
+
+                            
+                            
+                          
+                            
+                        ?>
+                         </tbody>
+            </table>  
+
+          </div>
+
           <div class="col  bg-danger ms-2 me-2 p-2 text-dark bg-opacity-50 rounded">
             <h1 class="text-center">Low on Stocks<h1>
 
@@ -156,10 +262,116 @@ if ($branchID_Check>0) {                                               #username
           </div>
       </div>
     </div>
+
+    <script type="text/javascript">
+        function showManager(adminID){
+
+            $.post("displayItems.php",{adminID:adminID},function(data,status){
+                var json=JSON.parse(data);
+                document.getElementById("branchInfo").innerHTML = json.map(getInfo).join("");
+                function getInfo(info) {
+                return "<tr><td>"+ info.admin_Username + "</td><td>"+ info.contact + "</td></tr>";
+                }
+                // document.getElementById("demo").innerHTML = myJSON;
+                  //  alert("Data: " + data );
+            });
+           
+            $('#showInfo').modal('show');
+        };
+
+        function managerForm(){
+            $('#newManager').modal('show');
+
+        };
+    </script>
   
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+     <!-- show items modal ##################################-->
+     <div class="modal fade" id="showInfo" tabindex="-1" aria-labelledby="showInfoLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="showInfoLabel">Branch Managers Information</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-hover ">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Contact</th>
+                        </tr>
+                    </thead>
+                    <tbody id="branchInfo" >
+                      
 
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+            </div>
+            </div>
+        </div>
+    </div>
+
+ <!-- New Item Modal ##################################-->
+ <div class="modal fade" id="newManager" tabindex="-1" aria-labelledby="newManagerModalLabel"  aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="newManagerModalLabel">New Branch Manager Form</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form class="row g-3 pb-4" action="addItem.php" method="post">
+                                    <div class="col-12">
+                                        <label for="ItemName" class="form-label">Branch Name</label>
+                                        <input type="text" class="form-control" name="ItemName" required>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <label for="Retail_Price" class="form-label">Branch Address</label>
+                                        <input type="text" class="form-control" name="Category"required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="Brand" class="form-label">Branch Contact</label>
+                                        <input type="text" class="form-control" name="Brand" required>
+                                    </div>
+                                    <div class="col-6">
+                                        <label for="ItemName" class="form-label">Admin Username</label>
+                                        <input type="text" class="form-control" name="ItemName" required>
+                                    </div>
+
+
+                                    <div class="col-md-6">
+                                        <label for="Retail_Price" class="form-label">Admin Password</label>
+                                        <input type="text" class="form-control" name="Category"required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label for="Brand" class="form-label">Admin Contact</label>
+                                        <input type="text" class="form-control" name="Brand" required>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <label for="adminPass" class="form-label text-dark">Enter Password to Confirm Action</label>
+                                        <input type="password" class="form-control" name="AdminPass" required>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-dark" name="Add">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>          
   </body>
 </html>
