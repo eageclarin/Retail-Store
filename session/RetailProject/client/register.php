25ecca9cb639246445b2a027623009ee5f7aebc3
JaeCamp00 <jlcampo@up.edu.ph>
2021-12-17 20:01:00 +0800

    client.css

--- session/RetailProject/client/register.php
+++ session/RetailProject/client/register.php
@@ -68,6 +68,7 @@
 <title> Register </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="client.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
